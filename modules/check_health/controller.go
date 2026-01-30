001b48faaae14f9948b45cd3b2d4b4e024eab7c3
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2026-01-31 06:57:07 +0700

    fix: check health content-type

--- modules/check_health/controller.go
+++ modules/check_health/controller.go
@@ -6,6 +6,8 @@ import (
 )
 
 func CheckHealthController(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(map[string]any{
 		"status":  "ok",
 		"message": "Application is running",
